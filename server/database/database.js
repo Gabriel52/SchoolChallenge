3a715e7912fe3c911855f0665d5367c20c4400a5
Gabriel52 <gabrielbrune52@gmail.com>
2021-01-13 16:26:19 -0300

    'update'

--- server/database/database.js
+++ server/database/database.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 //###### Variavel de conexão
-const connection = new Sequelize("SchoolChallenge","root","",{
-    host: "localhost",
+const connection = new Sequelize(process.env.MYSQL_DATABASE_NAME,process.env.MYSQL_DATABASE_USER,process.env.MYSQL_DATABASE_PASSWORD,{
+    host: process.env.MYSQL_DATABASE_HOST,
     dialect: 'mysql',
     timezone: '-03:00'
 });
